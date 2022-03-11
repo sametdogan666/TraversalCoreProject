d5bca1ed0d9117219c1d3b4fcb69447be252b70d
Samet DOGAN <s.doqann1570@gmail.com>
2022-03-11 14:18:47 +0300

    Feature Statistics Alanlari Duzenlendi

---
+++ BusinessLayer/Concrete/FeatureManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class FeatureManager : IFeatureService
+    {
+        private IFeatureDal _featureDal;
+
+        public FeatureManager(IFeatureDal featureDal)
+        {
+            _featureDal = featureDal;
+        }
+
+        public void Add(Feature t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Feature t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Feature t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Feature> GetList()
+        {
+            return _featureDal.GetList();
+        }
+
+        public Feature GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TraversalCoreProject/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _Feature : ViewComponent
+    {
+        FeatureManager _featureManager = new FeatureManager(new EfFeatureDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _featureManager.GetList();
+            return View(values);
+        }
+    }
+}
