5af1564fc8af0a451ca45e091d76755252b2c9a8
Samet DOGAN <s.doqann1570@gmail.com>
2022-05-02 14:28:09 +0300

    Tur Rotalari Sayfasi Duzenlendi

---
+++ TraversalCoreProject/Controllers/DestinationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using TraversalCoreProject.Models;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager _destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = _destinationManager.GetList();
+            return View(values);
+        }
+
+    }
+}
